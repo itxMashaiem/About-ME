e5ca759bcf8e1ae9f9918ea40c044c975678d773
itxMashaiem <mmashaiemnadeemryk80@gmail.com>
2024-09-30 12:57:07 +0500

    Add files via upload

---
+++ me.js
@@ -0,0 +1,65 @@
+
+function more() {
+    var x = document.getElementById("firstcontent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        var y = document.getElementById("third-content");
+        if (x.style.display = "block") {
+            y.style.display = "none";
+        }
+        var z = document.getElementById("sec-content");
+        if (x.style.display = "block") {
+            z.style.display = "none";
+        }
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function secmore() {
+    var x = document.getElementById("sec-content");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        var y = document.getElementById("firstcontent");
+        if (x.style.display = "block") {
+            y.style.display = "none";
+        }
+        var z = document.getElementById("third-content");
+        if (x.style.display = "block") {
+            z.style.display = "none";
+        }
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function thirdmore() {
+    var x = document.getElementById("third-content");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        var y = document.getElementById("firstcontent");
+        if (x.style.display = "block") {
+            y.style.display = "none";
+        }
+        var z = document.getElementById("sec-content");
+        if (x.style.display = "block") {
+            z.style.display = "none";
+        }
+
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+
+function mas() {
+    var x = document.getElementById('me');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+
+}
